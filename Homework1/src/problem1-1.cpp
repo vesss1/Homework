b24afff56a114004668415e1406bd69018da73da
vesss1 <41343127@nfu.edu.tw>
2025-10-13 13:40:02 +0800

    Update problem1-1.cpp

--- Homework1/src/problem1-1.cpp
+++ Homework1/src/problem1-1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int A(int x, int y) {
+    if (x == 0)
+        return y + 1;
+    else if (y == 0)
+        return A(x - 1, 1);
+    else
+        return A(x - 1, A(x, y - 1));
+}
+
+int main() {
+    int x, y;
+    cout << "Enter x and y: ";
+    cin >> x >> y;
+    cout << "Ackermann(" << x << "," << y << ") = " << A(x, y) << endl;
+    return 0;
+}
 
