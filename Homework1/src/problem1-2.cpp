0d3df7cfc6b46e325722e64ecdd211deb00b7ee1
vesss1 <41343127@nfu.edu.tw>
2025-10-13 13:46:54 +0800

    Update problem1-2.cpp

--- Homework1/src/problem1-2.cpp
+++ Homework1/src/problem1-2.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void powerSet(vector<char> z, vector<char> cur, int x) {
-    if (x == z.size()) {
-        cout << "{";
-        for (int i = 0; i < cur.size(); i++) {
-            cout << cur[i];
-            if (i < cur.size() - 1) cout << ",";
+int A(int x, int y) {
+    int sx[10000], sy[10000]; // 模擬堆疊
+    int top = 0;
+    sx[top] = x;
+    sy[top] = y;
+
+    while (top >= 0) {
+        x = sx[top];
+        y = sy[top];
+        top--;
+
+        if (x == 0) {
+            y = y + 1;
+        } 
+        else if (y == 0) {
+            top++;
+            sx[top] = x - 1;
+            sy[top] = 1;
+            continue;
+        } 
+        else {
+            top++;
+            sx[top] = x - 1;
+            sy[top] = -1; // 標記等內層結果
+            top++;
+            sx[top] = x;
+            sy[top] = y - 1;
+            continue;
         }
-        cout << "}" << endl;
-        return;
+
+        // 模擬回傳階段
+        while (top >= 0 && sy[top] == -1) {
+            x = sx[top];
+            top--;
+            top++;
+            sx[top] = x;
+            sy[top] = y;
+            break;
+        }
     }
-    // 不包含 z[x]
-    powerSet(z, cur, x + 1);
-    // 包含 z[x]
-    cur.push_back(z[x]);
-    powerSet(z, cur, x + 1);
+    return y;
 }
 
 int main() {
-    vector<char> z = {'a', 'b', 'c'};
-    vector<char> cur;
-    powerSet(z, cur, 0);
+    int x, y;
+    cout << "Enter x and y: ";
+    cin >> x >> y;
+    cout << "Ackermann(" << x << "," << y << ") = " << A(x, y) << endl;
     return 0;
 }
-
