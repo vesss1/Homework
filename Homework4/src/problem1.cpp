97da3d5c00ae0cad75e020e178a0f3511c20da15
Vesper <41343127@nfu.edu.tw>
2026-03-31 16:58:46 +0800

    Refactor Heap class to use dynamic array

--- Homework4/src/problem1.cpp
+++ Homework4/src/problem1.cpp
@@ -1,134 +1,151 @@
+//41343127
 #include <iostream>
-#include <vector>
-#include <stdexcept>
+#include <string>
 using namespace std;
 
 template <class T>
 class Heap {
 private:
-    vector<T> h;
+    T* h;
+    int size;
+    int capacity;
     bool isMin;
 
-    bool cmp(const T& a, const T& b) const {
-        return isMin ? a < b : a > b;
+public:// 建構子
+    Heap(int c, bool m) {
+        capacity = c;
+        size = 0;
+        isMin = m;
+        h = new T[capacity];
     }
 
-    void up(int i) {
-        while (i > 0) {
-            int p = (i - 1) / 2;
-            if (cmp(h[i], h[p])) {
-                swap(h[i], h[p]);
-                i = p;
-            }
-            else {
-                break;
-            }
+    ~Heap() {// 解構子
+        delete[] h;
+    }
+
+    bool IsEmpty() const {
+        if (size == 0) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
-    void down(int i) {
-        int n = h.size();
-        while (2 * i + 1 < n) {
+    T Top() const {// MinHeap 回傳最小值，MaxHeap 回傳最大值
+        return h[0];
+    }
+
+    void Push(T x) {// 插入新元素
+        if (size >= capacity) {
+            return;
+        }
+
+        h[size] = x;// 先把新元素放到最後面
+        int i = size;
+        size++;
+
+        while (i > 0) {
+            int p = (i - 1) / 2;// 父節點位置
+
+            if (isMin) {// MinHeap：如果子節點比父節點小，就交換
+                if (h[i] < h[p]) {
+                    T temp = h[i];
+                    h[i] = h[p];
+                    h[p] = temp;
+                    i = p;
+                }
+                else {
+                    break;
+                }
+            }
+            else {// MaxHeap：如果子節點比父節點大，就交換
+                if (h[i] > h[p]) {
+                    T temp = h[i];
+                    h[i] = h[p];
+                    h[p] = temp;
+                    i = p;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+  // 刪除頂端元素  :<
+    void Pop() {
+        if (size == 0) {
+            return;
+        }
+
+        h[0] = h[size - 1];
+        size--;
+
+        int i = 0;
+
+        while (2 * i + 1 < size) {
             int l = 2 * i + 1;
             int r = 2 * i + 2;
             int t = l;
 
-            if (r < n && cmp(h[r], h[l])) {
-                t = r;
-            }
+            if (isMin) {
+                if (r < size && h[r] < h[l]) {
+                    t = r;
+                }
 
-            if (cmp(h[t], h[i])) {
-                swap(h[t], h[i]);
-                i = t;
+                if (h[t] < h[i]) {
+                    T temp = h[i];
+                    h[i] = h[t];
+                    h[t] = temp;
+                    i = t;
+                }
+                else {
+                    break;
+                }
             }
             else {
-                break;
+                if (r < size && h[r] > h[l]) {
+                    t = r;
+                }
+
+                if (h[t] > h[i]) {
+                    T temp = h[i];
+                    h[i] = h[t];
+                    h[t] = temp;
+                    i = t;//:<
+                }
+                else {
+                    break;
+                }
             }
-        }
-    }
-
-public:
-    Heap(bool m = true) : isMin(m) {}
-
-    bool Empty() const {
-        return h.empty();
-    }
-
-    const T& Top() const {
-        if (Empty()) {
-            throw runtime_error("Heap is empty!");
-        }
-        return h[0];
-    }
-
-    void Push(const T& v) {
-        h.push_back(v);
-        up(h.size() - 1);
-    }
-
-    void Pop() {
-        if (Empty()) {
-            throw runtime_error("Heap is empty!");
-        }
-        swap(h[0], h.back());
-        h.pop_back();
-        if (!Empty()) {
-            down(0);
-        }
-    }
-
-    void in_p(int n) {
-        T x;
-        for (int i = 0; i < n; i++) {
-            cin >> x;
-            Push(x);
-        }
-    }
-
-    void printTree() {
-        int n = h.size();
-        int lv = 0;
-        int idx = 0;
-
-        while (idx < n) {
-            int cnt = 1 << lv;
-            cout << "L" << lv << ": ";
-            for (int i = 0; i < cnt && idx < n; i++) {
-                cout << h[idx++] << " ";
-            }
-            cout << endl;
-            lv++;
         }
     }
 };
 
 int main() {
-    int n;
-
-    cout << "請輸入測資組數: ";
+    int n, x;
     cin >> n;
 
-    Heap<int> mn(true);
-    cout << "請輸入" << n << "個測資數 (MinHeap):" << endl;
-    mn.in_p(n);
+    Heap<int> minHeap(n, true);
+    Heap<int> maxHeap(n, false);
 
-    cout << endl << "MinHeap" << endl;
-    mn.printTree();
-    cout << endl << "最小元素: " << mn.Top() << endl;
-    mn.Pop();
-    cout << "刪除最小元素後的樹:" << endl;
-    mn.printTree();
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        minHeap.Push(x);
+        maxHeap.Push(x);//41343127
+    }
 
-    Heap<int> mx(false);
-    cout << endl << "請輸入" << n << "個測資數 (MaxHeap):" << endl;
-    mx.in_p(n);
+    cout << "MinHeap" << endl;
+    cout << "IsEmpty: " << minHeap.IsEmpty() << endl;
+    cout << "Top: " << minHeap.Top() << endl;
+    minHeap.Pop();
+    cout << "After Pop, Top: " << minHeap.Top() << endl;
 
-    cout << endl << "MaxHeap" << endl;
-    mx.printTree();
-    cout << endl << "最大元素: " << mx.Top() << endl;
-    mx.Pop();
-    cout << "刪除最大元素後的樹:" << endl;
-    mx.printTree();
+    cout << "MaxHeap" << endl;//f
+    cout << "IsEmpty: " << maxHeap.IsEmpty() << endl;
+    cout << "Top: " << maxHeap.Top() << endl;
+    maxHeap.Pop();
+    cout << "After Pop, Top: " << maxHeap.Top() << endl;
 
     return 0;
 }
